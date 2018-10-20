b3a98148592535e9264b5e4a7dc08a774e639a5e
Matthew Cone <mcc67@duke.edu>
2018-10-20 00:50:39 -0400

    Final Push

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -3,18 +3,35 @@ import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class EfficientMarkov extends BaseMarkov {
+	/*
+	 * Initializes a Hashmap to use to store the characters based
+	 * on the markov order.
+	 */
 	private HashMap <String, ArrayList<String>> myMap;
-
+	/*
+	 * Makes a parent class and initilizes the hashmap myMap.
+	 */
 	public EfficientMarkov(int order) {
 		super(order);
 		myMap = new HashMap<String, ArrayList<String>>();
 	}
+	/*
+	 * Sets the Markov order to 3
+	 */
 	public EfficientMarkov() {
 		this(3);
 	}
+	/*
+	 * adds keys and values to the hashmap. 
+	 * keys are a number of characters based on the order number
+	 * and the loop adds a number of characters to the key then
+	 * adds the next three letters as the proceeding key. (abc to
+	 * bcd). The values are the characters within each key split. 
+	 */
 	@Override
 	public void setTraining(String text) {
 		myText = text;
+		myMap.clear();
 		for(int i=0; i < myText.length() - myOrder + 1; i++) {
 			String wkey = myText.substring(i, i + myOrder);
 			if(! myMap.containsKey(wkey)) {
@@ -29,9 +46,12 @@ public class EfficientMarkov extends BaseMarkov {
 			}
 		}
 	}
+	/*
+	 * Returns the value of the key if the key is in the hashmap
+	 */
 	@Override
 	public ArrayList<String> getFollows(String key) {
-		if(!myMap.containsKey(key)) {
+		if(! myMap.containsKey(key)) {
 			throw new NoSuchElementException(key+" not in map");
 		}
 
--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -3,15 +3,29 @@ import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class EfficientWordMarkov extends BaseWordMarkov {
+	/*
+	 * Initializes a Hashmap to use to store the characters based
+	 * on the markov order.
+	 */
 	private HashMap <WordGram, ArrayList<String>> myMap;
-
+	/*
+	 * Makes a parent class and initilizes the hashmap myMap.
+	*/
 	public EfficientWordMarkov(int order) {
 		super(order);
 		myMap = new HashMap<WordGram, ArrayList<String>>();
 	}
+	/*
+	 * Sets the Markov order to 2
+	 */
 	public EfficientWordMarkov() {
 		this(2);
 	}
+	/*
+	 * adds keys and values to the hashmap. 
+	 * Functions the same way as setTraining for EfficientMarkov
+	 * but has a wordgram as the key instead of a string
+	 */
 	@Override
 	public void setTraining(String text) {
 		myMap.clear();
@@ -30,6 +44,9 @@ public class EfficientWordMarkov extends BaseWordMarkov {
 			}
 		}
 	}
+	/*
+	 * Returns the value of the key if the key is in the hashmap
+	 */
 	@Override
 	public ArrayList<String> getFollows(WordGram key) {
 
